d5e3ee1ef71fe356af359cbc52e3a75b595e6a94
Maulany <christian.maulany@kpn.com>
2021-12-02 11:31:07 +0100

    day1

---
+++ day1/part1.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+
+const input = fs.readFileSync(path.resolve(__dirname, 'input.txt'), 'utf8');
+const data = input.split('\n').map(n => Number(n));
+
+const measurements = data.map((n, index) => {
+    if (index === 0) {
+        return null;
+    }
+    const previousN = data[index - 1];
+    return previousN < n ? 'increased' : 'decreased';
+});
+
+const answer = measurements.filter(measurement => measurement === "increased").length;
+
+console.log(`Answer: ${answer}`);
---
+++ day1/part2.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+
+const input = fs.readFileSync(path.resolve(__dirname, 'input.txt'), 'utf8');
+const data = input.split('\n').map(n => Number(n));
+
+const windows = [];
+for (let i = 0; i < data.length - 2; i++) {
+    const window = data[i] + data[i + 1] + data[i + 2];
+    windows.push(window);
+}
+
+const measurements = windows.map((n, index) => {
+    if (index === 0) {
+        return null;
+    }
+    const previousN = windows[index - 1];
+    return previousN < n ? 'increased' : 'decreased';
+});
+
+const answer = measurements.filter(measurement => measurement === "increased").length;
+
+console.log(`Answer: ${answer}`);
