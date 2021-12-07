f49eeb45f2f458a6ef18fa5981f8f6820d664d04
Maulany <christian.maulany@kpn.com>
2021-12-07 10:02:41 +0100

    renamed i to position.

--- day7/part1.js
+++ day7/part1.js
@@ -9,9 +9,9 @@ const min = crabs.reduce((min, n) => Math.min(min, n));
 const max = crabs.reduce((max, n) => Math.max(max, n));
 
 const positionToFuel = {};
-for (let i = min; i <= max; i++) {
-    const fuel = crabs.reduce((fuel, crab) => fuel + Math.abs(i - crab), 0);
-    positionToFuel[i] = fuel;
+for (let position = min; position <= max; position++) {
+    const fuel = crabs.reduce((fuel, crab) => fuel + Math.abs(position - crab), 0);
+    positionToFuel[position] = fuel;
 }
 
 const answer = Object.values(positionToFuel).reduce((best, fuel) => Math.min(best, fuel));
